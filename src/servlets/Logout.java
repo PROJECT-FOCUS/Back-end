941e7bbbfe3e08b07c2f2d8530735b1192943445
Haoxuan Yang <427725700@qq.com>
2019-12-23 16:23:14 -0700

    Servlets Login, Logout and Register modified and tested PS: changed password in MySQLDBUtil.java to '' (please change it back to 'root' if needed)

--- src/servlets/Logout.java
+++ src/servlets/Logout.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Logout
@@ -27,7 +28,11 @@ public class Logout extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			session.invalidate();
+		}
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append("/WebContent/index.html");
 	}
 
 	/**
--- src/servlets/Register.java
+++ src/servlets/Register.java
@@ -7,6 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
+
 /**
  * Servlet implementation class Register
  */
@@ -25,17 +30,38 @@ public class Register extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		DBConnection connection = DBConnectionFactory.getConnection();
+		try {
+			// convert request body into JSONobject "input"
+			JSONObject input = JsonHelper.readJSONObject(request);
+			String userId = input.getString("user_id");
+			String password = input.getString("password");
+			String firstname = input.getString("first_name");
+			String lastname = input.getString("last_name");
+			
+			// write user info into the database and create JSONobject "obj" for the feedback
+			JSONObject obj = new JSONObject();
+			if (connection.registerUser(userId, password, firstname, lastname)) {
+				obj.put("status", "OK");
+			} else {
+				obj.put("status", "User Already Exists");
+			}
+			JsonHelper.writeJsonObject(response, obj);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			connection.close();
+		}
 	}
 
 }
