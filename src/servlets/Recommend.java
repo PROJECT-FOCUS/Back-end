2667c089cfd577c5bf0bc1dde1aeba1b276b1029
Peng Ding <ding96@purdue.edu>
2019-12-26 11:31:34 -0500

    Update Recommend.java

--- src/servlets/Recommend.java
+++ src/servlets/Recommend.java
@@ -1,11 +1,19 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+
+import App.app;
+import services.RecommendService;
 
 /**
  * Servlet implementation class Recommend
@@ -35,7 +43,18 @@ public class Recommend extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			response.setStatus(403);
+			return;
+		}
+		String userId = session.getAttribute("user_id").toString();
+		RecommendService recommendation = new RecommendService();
+		List<app> apps = recommendation.recommend(userId);
+		JSONArray array = new JSONArray();
+		for (app a : apps) {
+			array.put(a.toJSONObject());
+		}
+		JsonHelper.writeJsonArray(response, array);
 	}
-
 }
